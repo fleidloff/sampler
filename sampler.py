c226b194dc09726a0fab8750889e3662d6210ce7
Frederik Leidloff <fleidloff@team.mobile.de>
2016-12-09 17:28:54 +0100

    make playable

--- sampler.py
+++ sampler.py
@@ -10,16 +10,28 @@ beat = pygame.mixer.Sound("beat.wav")
 # put sounds in array
 # load from sd / zoom
 # loop yes / no 
+# load from ditto?
+
+loop = False
+
+def play (sound):
+    sound.play(-1 if loop else 0)
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
+            pygame.mixer.quit()
             pygame.quit()
             quit()
         if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_l:
+                loop = True
             if event.key == pygame.K_0:
                 pygame.mixer.stop()    
             if event.key == pygame.K_1:
-                crash.play()
+                play(crash)
             if event.key == pygame.K_2:
-                beat.play()
+                play(beat)
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_l:
+                loop = False
