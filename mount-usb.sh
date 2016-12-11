01019839eb2c5439bac639ac474e4c047ebd6601
Frederik Leidloff <kontakt@frederik-leidloff.de>
2016-12-11 03:44:26 +0100

    fix copy script

--- mount-usb.sh
+++ mount-usb.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 sudo mount -t vfat /dev/sda1 /mnt/usb
 rm -r ./sounds/*
-#find /mnt/usb/ -maxdepth 1 -type f |head -1000|xargs cp -t ./sounds/
 
-ls ./tsounds/ | cat -n | head -n 8 |  while read n f; do cp "./tsounds/$f" "./sounds/$n.wav"; done 
+ls /mnt/usb/ | cat -n | head -n 8 |  while read n f; do cp "/mnt/usb/$f" "./sounds/$n.wav"; done 
 
 sudo chown -R pi:pi ./sounds
