5f0a6fa55a4f78605180fb85f6bcc46d1d5eed8d
Frederik Leidloff <kontakt@frederik-leidloff.de>
2016-12-11 03:40:36 +0100

    copy 8 files and rename

--- mount-usb.sh
+++ mount-usb.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 sudo mount -t vfat /dev/sda1 /mnt/usb
 rm -r ./sounds/*
-cp -r /mnt/usb/* ./sounds/
+#find /mnt/usb/ -maxdepth 1 -type f |head -1000|xargs cp -t ./sounds/
+
+ls ./tsounds/ | cat -n | head -n 8 |  while read n f; do cp "./tsounds/$f" "./sounds/$n.wav"; done 
+
 sudo chown -R pi:pi ./sounds
