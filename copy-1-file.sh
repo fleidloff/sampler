c1cc2ebc468acf803bd8ec762a0e0a435692938b
Frederik Leidloff <kontakt@frederik-leidloff.de>
2016-12-11 04:18:19 +0100

    fix typo

--- copy-1-file.sh
+++ copy-1-file.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 sudo mount -t vfat /dev/sda1 /mnt/usb
 
-ls /mnt/usb/ | cat -n | head -n 1 | while read n f; do cp " /mnt/usb/$f" "./sounds/$1.wav"; done 
+ls /mnt/usb/ | cat -n | head -n 1 | while read n f; do cp "/mnt/usb/$f" "./sounds/$1.wav"; done 
 
 sudo chown -R pi:pi ./sounds
